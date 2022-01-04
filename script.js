263e6424ba8a9d3f968e6e4ef0269bd5ea6fe438
David Kim <cjoak1028@gmail.com>
2022-01-04 11:39:52 -0500

    implemented highscore

--- script.js
+++ script.js
@@ -12,11 +12,13 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
+let highScore = 0;
+
 // document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+    // const guess = Number(document.querySelector('.guess').value);
+    // console.log(guess, typeof guess);
 
     // When there is no input
     if (!guess) {
@@ -29,6 +31,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
 
         // When guess is too high
     } else if (guess > secretNumber) {
